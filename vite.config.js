aa62e0ae47e818243248b239f257a27cc6cbfa66
Kaashf Ali <kashafali923@example.com>
2025-07-05 16:50:01 +0500

    ready for deployment

--- vite.config.js
+++ vite.config.js
@@ -4,10 +4,14 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
+
+const repoName = 'vue-api-integration-project'
+
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
+    base: `/${repoName}/`,
     vueDevTools(),
   ],
   resolve: {
